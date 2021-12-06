960073af53716c4f2afdc8b72b691e22b337a1f7
Gabritorre <gabritorredev@gmail.com>
2021-12-06 13:41:45 +0100

    Add day4 part1

---
+++ day4/part1/bingo.js
@@ -0,0 +1,77 @@
+let all_numbers = document.getElementsByTagName('h1')[0].textContent
+let numbers = all_numbers.split(',');
+// console.log(numbers)
+
+// creating an array of matrix
+let arrayMatrix = new Array(100)
+for(let i = 0; i < 100; i++){
+	arrayMatrix[i] = Array(5).fill(1);
+	for(let j = 0; j < 5; j++){
+		arrayMatrix[i][j] = Array(5).fill("1");
+	}
+}
+
+AllrowMatrix = document.getElementsByTagName('p')
+let element = 0;	//rappresent the index of a line for example the index of: <p>30 46 94 20 2</p>
+for(matrix = 0; matrix < 100; matrix++){
+	for(let row = 0; row < 5; row++, element++){
+		arrayRow = AllrowMatrix[element].textContent.split(' ') //transform "30 46 94 20 2" in an array of string
+		for(let i = 0; i < 5; i++){
+			arrayMatrix[matrix][row][i] = arrayRow[i];
+		}
+	}
+}
+var matrixWinnner;
+var numberWinner;
+
+go_on = true;
+for(let num = 0; num < numbers.length && go_on; num++){
+	for(matrix = 0; matrix < 100 && go_on; matrix++){
+		for(let row = 0; row < 5 && go_on; row++, element++){
+			for(let i = 0; i < 5 && go_on; i++){
+				if(arrayMatrix[matrix][row][i] == numbers[num]){
+					arrayMatrix[matrix][row][i] = "X";
+				}
+			}
+		}
+	}
+	numberWinner = numbers[num];
+	go_on = checkWinner(arrayMatrix);
+}
+
+let result = 0;
+for(let row = 0; row < 5; row++){
+	for (let i = 0; i < 5; i++){
+		if(arrayMatrix[matrixWinnner][row][i] != "X"){
+			result += Number(arrayMatrix[matrixWinnner][row][i])
+		}
+	}
+}
+
+console.log(result * numberWinner)
+
+function checkWinner(arrayMatrix){
+	let counterRow = 0;
+	let counterColumn = 0;
+	let go_on = true;
+
+	for(matrix = 0; matrix < 100 && go_on; matrix++){
+		for(let row = 0; row < 5 && go_on; row++){
+			for(let i = 0; i < 5 && go_on; i++){
+				if(arrayMatrix[matrix][row][i] == "X"){
+					counterRow++
+				}
+				if(arrayMatrix[matrix][i][row] == "X"){
+					counterColumn++
+				}
+			}
+			if(counterRow == 5 || counterColumn == 5){
+				go_on = false;
+				matrixWinnner = matrix;
+			}
+			counterRow = 0;
+			counterColumn = 0;
+		}
+	}
+	return go_on
+}
